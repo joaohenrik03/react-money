2b9027c1d5edde64541f48a462018e9cb5807d42
João Henrik <jh.signori@gmail.com>
2022-06-29 19:07:58 -0300

    Transitions table created and added to the dashboard

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./style";
+
+export function TransactionTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Preço</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de site</td>
+                        <td className="deposit">R$ 12.000,00</td>
+                        <td>Venda</td>
+                        <td>13/04/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Hamburguer</td>
+                        <td className="withdrawal">- R$ 59,00</td>
+                        <td>Alimentação</td>
+                        <td>10/04/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
