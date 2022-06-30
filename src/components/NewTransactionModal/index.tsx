1ef030ef97a1c64d6c36446cec0870a675388764
João Henrik <jh.signori@gmail.com>
2022-06-30 16:46:35 -0300

    Modal structure created

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from "react-modal";
 
+import { Container } from './style';
+
 type NewTransactionModalProps = {
     isOpen: boolean;
     onRequestClose: () => void;
@@ -10,8 +12,29 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName='react-modal-overlay'
+            className='react-modal-content'
         >
-            <h2>Cadastrar transação</h2>
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input 
+                    type="text" 
+                    placeholder="Título"
+                />
+                <input 
+                    type="number" 
+                    placeholder="Preço"
+                />
+                <input 
+                    type="text" 
+                    placeholder="Categoria"
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     )
 }
