0d9798570b8066889ed7152bc8bf401516f371df
João Henrik <jh.signori@gmail.com>
2022-06-30 16:14:01 -0300

    React modal configured

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,34 @@ import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 
+import { useState } from "react";
+
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
+
   return (
     <>
-      <Header />
-      <Dashboard /> 
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
 import Logo from '../../assets/logo.svg';
+
 import { Container, Wrapper } from './style';
 
-export function Header() {
+type HeaderProps = {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Wrapper>
@@ -12,7 +17,7 @@ export function Header() {
                     />
                     <span>React Money</span>
                     </div>
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Wrapper>
