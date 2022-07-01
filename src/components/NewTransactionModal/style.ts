901f88a6b3a1511ffc3bb40f7036f520e4e9304d
João Henrik <jh.signori@gmail.com>
2022-07-01 14:49:57 -0300

    Stylized modal

--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -1,5 +1,51 @@
 import styled from "styled-components";
 
 export const Container = styled.form`
+    h2 {
+        color: var(--titles);
+        font-size: 1.5rem;
 
+        margin-bottom: 2rem;   
+    }    
+
+    input {
+        width: 100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+
+        border-radius: .25rem;
+        border: 1px solid #D7D7D7;
+        background: #E7E9EC;
+
+        font-weight: 400;
+        font-size: 1rem;
+
+        &::placeholder {
+            color: var(--texts)
+        }
+
+        & + input {
+            margin-top: 1rem;
+        }
+    }
+
+    button[type="submit"] {
+        width: 100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+
+        background: var(--green);
+        border-radius: .25rem;
+        border: 0;
+
+        color: var(--shape);
+        font-size: 1rem;
+        font-weight: 600;
+
+        margin-top: 1.5rem;
+        transition: filter 0.3s;
+        &:hover {
+            filter: brightness(0.9);
+        }   
+    }
 ` 
