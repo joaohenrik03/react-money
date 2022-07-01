b225d965d9cb9ea03255cb4fdea77685d1c1394a
João Henrik <jh.signori@gmail.com>
2022-07-01 15:02:32 -0300

    Input and output buttons created in the modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
-import { Container } from './style';
+import { ChooseTransitionTypeContainer, Container } from './style';
 
 type NewTransactionModalProps = {
     isOpen: boolean;
@@ -35,6 +37,19 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                     type="number" 
                     placeholder="Preço"
                 />
+
+                <ChooseTransitionTypeContainer>
+                    <button type="button" >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+                    
+                    <button type="button" >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </ChooseTransitionTypeContainer>
+
                 <input 
                     type="text" 
                     placeholder="Categoria"
